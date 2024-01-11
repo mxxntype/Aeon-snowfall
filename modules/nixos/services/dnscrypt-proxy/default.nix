1bd99fbb3814b4ef04eebcecac3f734178332f83
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-12 02:16:45 +0300

    feat(modules/nixos): Add `dnscrypt-proxy` service

---
+++ modules/nixos/services/dnscrypt-proxy/default.nix
@@ -0,0 +1,49 @@
+{
+    config,
+    lib,
+    ...
+}:
+
+with lib; {
+    options.aeon.services.DNSCryptProxy = {
+        enable = mkOption {
+            description = "Whether to enable the dnscrypt-proxy2 service";
+            type = types.bool;
+            default = true;
+        };
+        settings = mkOption {
+            type = types.attrs;
+            default = {
+                ipv4_servers = true;
+                ipv6_servers = true;
+                dnscrypt_servers = true;
+                doh_servers = true;
+                require_dnssec = true;
+                require_nolog = true;
+                require_nofilter = true;
+                block_ipv6 = false;
+
+                timeout = 5000;
+                keepalive = 30;
+                lb_strategy = "p2";
+                lb_estimator = true;
+                cache = true;
+                force_tcp = false; # No effect if tor is not in use
+
+                bootstrap_resolvers = [ "1.1.1.1:53" ];
+                netprobe_address = "1.1.1.1:53";
+                captive_portals.map_file = "/etc/dnscrypt-proxy/captive_portals.txt";
+            };
+        };
+    };
+
+    config = let
+        cfg = config.aeon.services.DNSCryptProxy;
+    in mkIf cfg.enable {
+        services.dnscrypt-proxy2 = {
+            enable = true;
+            inherit (cfg) settings;
+            upstreamDefaults = true;
+        };
+    };
+}
