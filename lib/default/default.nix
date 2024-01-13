87f1762143f3e454a8190f6542f35354bd6d2d00
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-13 13:57:57 +0300

    feat(lib): Add `aeon.user` variable

--- lib/default/default.nix
+++ lib/default/default.nix
@@ -5,4 +5,9 @@
     nix-std = builtins.attrValues inputs.nix-std.lib;
 in {
     inherit nix-std;
+
+    # INFO: I change my username from time to time,
+    # An because some NixOS options inherit from from my Home-manager options,
+    # I think it's quite reasonable to have my username declared as a variable.
+    user = "mxxntype";
 }
