f1ea4e3d36d8dae3e4d6731fefdd999320a171b5
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-13 23:17:49 +0300

    feat(modules): Add `ssh` NixOS module

---
+++ modules/nixos/ssh/default.nix
@@ -0,0 +1,45 @@
+# INFO: SSH NixOS module.
+
+{
+    config,
+    lib,
+    ...
+}:
+
+with lib; {
+    options.aeon.ssh = {
+        enable = mkOption {
+            type = types.bool;
+            default = true;
+            description = "Whether to enable SSH";
+        };
+    };
+
+    config = mkIf config.aeon.ssh.enable {
+        services.openssh = {
+            enable = true;
+            settings = {
+                PasswordAuthentication = false;
+                PermitRootLogin = "no";
+                StreamLocalBindUnlink = "yes";    # Automatically remove stale sockets
+            };
+            # hostKeys = [
+            #     {
+            #         path = "/etc/ssh/ssh_host_ed25519_key";
+            #         type = "ed25519";
+            #     }
+            # ];
+        };
+
+        programs = {
+            ssh.startAgent = true;
+            mosh = {
+                enable = true;
+                withUtempter = true;
+            };
+        };
+
+        security.pam.enableSSHAgentAuth = true; # Passwordless sudo when SSH'ing with keys
+        environment.systemPackages = with pkgs; [ sshfs ];
+    };
+}
