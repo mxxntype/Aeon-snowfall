8e3a052298219e401ad79ead6ded805e1e72e600
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-13 20:07:45 +0300

    feat(modules): Add `boot` and `console` NixOS modules.

---
+++ modules/nixos/boot/default.nix
@@ -0,0 +1,107 @@
+# INFO: Linux console/TTY NixOS module. WARN: Mostly untested.
+
+{
+    config,
+    lib,
+    ...
+}:
+
+with lib; {
+    options.aeon.boot = {
+        type = mkOption {
+            type = types.enum [ "bios" "uefi" "lanzaboote" ];
+            default = "uefi";
+            description = "What type of bootloader module to use";
+        };
+
+        quiet = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Whether to enable Plymouth and reduce TTY verbosity";
+        };
+
+        grub.device = mkOption {
+            type = types.str;
+            default = "nodev";
+            description = /* md */ ''
+                Link to `boot.loader.grub.device`.
+                Set this to anything other than `"nodev"` only if using legacy BIOS boot.
+            '';
+        };
+    };
+
+    config = mkMerge [
+        { boot.loader.grub.enable = mkDefault true; } # Use GRUB2 by default.
+
+        (mkIf (config.aeon.boot.type == "bios") {
+            boot = {
+                loader = {
+                    grub = {
+                        efiSupport = mkDefault false;
+                        inherit (config.aeon.boot.grub) device;
+                    };
+                };
+            };
+        })
+
+        (mkIf (config.aeon.boot.type == "uefi") {
+            boot = {
+                loader = {
+                    grub = {
+                        efiSupport = true;
+                        efiInstallAsRemovable = mkDefault false;
+                    };
+
+                    efi = {
+                        efiSysMountPoint = mkDefault "/boot/efi";
+                        canTouchEfiVariables = true;
+                    };
+                };
+            };
+        })
+
+        # INFO: https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md
+        (mkIf (config.aeon.boot.type == "lanzaboote") {
+            environment.systemPackages = with pkgs; [ sbctl ];
+            boot = {
+                lanzaboote = {
+                    enable = true;
+                    pkiBundle = "/etc/secureboot";
+                };
+                loader = {
+                    # HACK: Lanzaboote currently replaces systemd-boot, so force it to false for now.
+                    systemd-boot.enable = mkForce false;
+                    grub.enable = mkForce false;
+                };
+            };
+        })
+        
+        (mkIf config.aeon.boot.quiet {
+            boot = {
+                plymouth = {
+                    enable = true;
+                    theme = "breeze";
+
+                    # TODO: Configure font & logo.
+                    # font = "${pkgs.nerdfonts.override { fonts = [ ]; }}/share/fonts/...";
+                    # logo = .../logo.png;
+                };
+
+                consoleLogLevel = 0;
+                kernelParams = [
+                    "quiet"
+                    "loglevel=3"
+                    "systemd.show_status=auto"
+                    "udev.log_level=3"
+                    "rd.udev.log_level=3"
+                    "vt.global_cursor_default=0"
+                ];
+
+                initrd = {
+                    systemd.enable = true;
+                    verbose = false;
+                };
+            };
+        })
+    ];
+}
---
+++ modules/nixos/console/default.nix
@@ -0,0 +1,61 @@
+# INFO: Linux console/TTY NixOS module.
+
+{
+    config,
+    lib,
+    ...
+}:
+
+with lib; {
+    options.aeon.console = {
+        enable = mkOption {
+            description = "Whether to configure the Linux TTY";
+            type = types.bool;
+            default = true;
+        };
+    };
+
+    config = mkMerge [
+        (mkIf config.aeon.console.enable {
+            console = {
+                earlySetup = mkDefault true;
+                colors = let
+                    inherit (config.aeon.theme) colors ui;
+                in [
+                    # Normal
+                    "000000"
+                    "${colors.red}"
+                    "${colors.green}"
+                    "${colors.yellow}"
+                    "${colors.blue}"
+                    "${colors.mauve}"
+                    "${colors.teal}"
+                    "${ui.fg.text}"
+                    # Bright
+                    "${ui.fg.subtext0}"
+                    "${colors.maroon}"
+                    "${colors.green}"
+                    "${colors.rosewater}"
+                    "${colors.sapphire}"
+                    "${colors.pink}"
+                    "${colors.sky}"
+                    "${ui.fg.text}"
+                ];
+            };
+
+            # NOTE: Attempt at a better TTY. WARN: Buggy.
+            # environment.systemPackages = with pkgs; [ fbterm zellij tmux ];
+            # fonts.packages = with pkgs; [
+            #     (nerdfonts.override {
+            #         fonts = [
+            #             "BigBlueTerminal"
+            #             "JetBrainsMono"
+            #             "IosevkaTerm"
+            #         ];
+            #     })
+            #     # corefonts
+            # ];
+        })
+    ];
+
+}
