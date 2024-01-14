5f509d5db0f99075009e8069938bf36998864d62
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-15 02:45:29 +0300

    chore(systems/illusion): Flatten `boot.*` options for parsing

--- systems/x86_64-linux/illusion/default.nix
+++ systems/x86_64-linux/illusion/default.nix
@@ -13,16 +13,12 @@
         };
     };
 
-    boot = {
-        initrd = {
-            kernelModules = [ ];
-            availableKernelModules = [ ];
-            systemd = {};
-        };
-
-        kernelModules = [ ];
-        extraModulePackages = [ ];
-    };
+    # NOTE: Flattened for the installer script.
+    boot.initrd.systemd = {};
+    boot.initrd.kernelModules = [ ];
+    boot.initrd.availableKernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
 
     system.stateVersion = "23.11"; # WARN: Changing this might break things. Just leave it.
 }
