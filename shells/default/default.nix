0ef39688ff5e9c6b491a7aa639df15c762a51bac
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-15 03:08:44 +0300

    feat(shells): Add `default` shell for bootstrapping

---
+++ shells/default/default.nix
@@ -0,0 +1,17 @@
+{
+    pkgs,
+    ...
+}:
+
+pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+    buildInputs = with pkgs; [
+        git
+        home-manager
+        helix
+        nushell
+        sops
+        age
+        ssh-to-age
+    ] ++ (builtins.attrValues aeon);
+}
