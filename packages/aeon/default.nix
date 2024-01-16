f1cb6283dd74c0f565daa3f5bc7bf0aa60d2bea4
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-16 21:21:08 +0300

    chore(packages/aeon): Add more comments & improve existing ones

--- packages/aeon/default.nix
+++ packages/aeon/default.nix
@@ -37,41 +37,48 @@ pkgs.nuenv.writeScriptBin {
             --create-fs (-c) # Create FS on the target drive.
             --mount (-m): directory = /mnt # Where to mount the target drive.
             --install (-I) # Run nixos-install.
-            --root-lv-size (-R): int = 100 # Size of the root LVM LV.(%)
+            --root-lv-size (-R): int = 100 # Size of the root LVM LV in %.
             --ignore-generated-config (-i) # Do not automatically inherit `boot.*` options from `nixos-generate-config`.
             --BIOS (-B) # Use legacy BIOS boot instead of UEFI.
         ]: nothing -> nothing {
+            # Run `fdisk` to partition a drive.
             if ($partition) {
                 let target_drive = (select_blockdev --type "disk" --hint "installation drive")
                 print $"Starting (ansi blue)fdisk(ansi reset) to partition target drive..."
                 sudo ${pkgs.util-linux}/bin/fdisk $target_drive
             }
 
+            # TODO: Implement filesystems other than BTRFS.
             if ($create_fs) {
                 let root_part = (select_blockdev --type "part" --hint "root partition")
 
+                # Set up LVM.
                 let vg = $hostname
                 print $"Setiing up (ansi blue_bold)LVM(ansi reset)..."
                 sudo vgcreate $vg $root_part
                 sudo lvcreate -n root -l $"($root_lv_size)%FREE" $vg
 
+                # Create BTFS.
                 print $"Creating (ansi default_bold)BTRFS(ansi reset) filesystem and subvolumes..."
                 sudo ${pkgs.btrfs-progs}/bin/mkfs.btrfs $root_part -L $"($hostname)_btrfs" -q
                 sudo mount $root_part $mount
 
+                # Select & create subvolumes.
                 const subvolumes: list<string> = ["@" "@home" "@nix" "@persist"]
                 let selected = ($subvolumes | input list --multi "What BTFS subvolumes to create")
-                for sv in $selected {
-                    sudo btrfs subvolume create $"($mount)/($sv)"
+                for subvolume in $selected {
+                    sudo btrfs subvolume create $"($mount)/($subvolume)"
                 }
                 sudo umount -R $mount
 
-                for sv in $selected {
-                    let subdir = ($sv | str trim -c "@")
+                # Mount each subvolume with options.
+                for subvolume in $selected {
+                    let subdir = ($subvolume | str trim -c "@")
                     if not ($subdir | is-empty) { sudo mkdir $"($mount)/($subdir)" }
-                    sudo mount $root_part $"($mount)/($subdir)" -o $"compress=zstd,space_cache=v2,subvol=($sv)"
+                    sudo mount $root_part $"($mount)/($subdir)" -o $"compress=zstd,space_cache=v2,subvol=($subvolume)"
                 }
 
+                # Create the UEFI partition if needed.
                 if not ($BIOS) {
                     let efi_part = (select_blockdev --type "part" --hint "EFI partition")
                     sudo mkfs.fat -F 32 -n EFI $efi_part
@@ -81,6 +88,7 @@ pkgs.nuenv.writeScriptBin {
                 }
             }
 
+            # Copy needed hardware-related options from generated config.
             if not ($ignore_generated_config) {
                 let options = sudo nixos-generate-config --root $mount --show-hardware-config
                     | lines
@@ -95,12 +103,14 @@ pkgs.nuenv.writeScriptBin {
                 }
             }
 
+            # Run `nixos-install`.
             if $install {
                 print $"Running (ansi red)nixos-install(ansi reset)..."
                 sudo ${pkgs.nixos-install-tools}/bin/nixos-install --root $mount --flake $".#($hostname)"
             }
         }
 
+        # Pick a drive/partition from the ones available.
         def select_blockdev [
             --type: string # `disk` or `part`
             --hint: string # Hint of what's happening for the user.
