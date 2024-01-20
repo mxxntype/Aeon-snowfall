da43dfce12b79d3d5d7d4019eb4532b59bb8ef6f
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-21 00:45:03 +0300

    feat(modules): Add `dev/rust` Home-manager module

---
+++ modules/home/dev/rust/default.nix
@@ -0,0 +1,56 @@
+# INFO: Home-manager Rust module.
+
+{
+    config,
+    lib,
+    pkgs,
+    ...
+}:
+
+with lib; {
+    options.aeon.dev.rust = {
+        enable = mkOption {
+            type = types.bool;
+            default = true;
+        };
+
+        type = mkOption {
+            type = types.enum [ "rustup" "fenix" ];
+            default = "rustup";
+        };
+    };
+
+    config = let
+        inherit (config.aeon.dev.rust) enable type;
+    in {
+        home.packages = with pkgs; mkMerge [
+            # Common cargo/rust packages.
+            (mkIf enable [
+                cargo-nextest    # Next-generation test runner for Rust projects.
+                cargo-watch      # Watch over Cargo project's source
+                cargo-info       # Show crates info from crates.io.
+                cargo-deps       # Build dependency graphs of Rust projects.
+                cargo-generate   # Generate a new Rust project by leveraging a git repository as a template.
+                cargo-tauri      # Build smaller, faster, and more secure desktop applications with a web frontend.
+                cargo-binutils   # Invoke the LLVM tools shipped with the Rust toolchain.
+                cargo-valgrind   # Runs valgrind and collects its output in a helpful manner.
+                cargo-flamegraph # Easy flamegraphs for Rust projects and everything else, without Perl or pipes <3
+
+                bacon      # Background rust code checker.
+                diesel-cli # Database tool for working with projects that use Diesel.
+                sqlx-cli   # SQLx's associated command-line utility.
+                sccache    # Ccache with Cloud Storage.
+            ])
+            
+            # Install rustup and let it do its thing.
+            (mkIf (enable && type == "rustup") [
+                rustup
+            ])
+
+            # Install Rust via the `fenix` flake.
+            (mkIf (enable && type == "fenix") [
+                # TODO
+            ])
+        ];
+    };
+}
