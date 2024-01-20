4b1950dde36e0a1c7421d4cf1c434ae7ae193092
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-20 23:12:54 +0300

    chore(templates/rust): Add `openssl` quickfix

--- templates/rust/packages/default/default.nix
+++ templates/rust/packages/default/default.nix
@@ -6,4 +6,8 @@
 
 (pkgs.callPackage inputs.naersk {}).buildPackage {
     src = ../..;
+
+    # NOTE: If your application uses OpenSSL (making the build process fail), try:
+    # nativeBuildInputs = with pkgs; [ pkg-config ];
+    # buildInputs = with pkgs; [ openssl ];
 }
