322784b1e1944b0a8086c0e28ee3c46bb9122c1b
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-21 16:33:36 +0300

    feat(lib): Add `flakePath` variable

--- lib/default/default.nix
+++ lib/default/default.nix
@@ -3,20 +3,23 @@
     ...
 }: let
     nix-std = builtins.attrValues inputs.nix-std.lib;
-in {
+in rec {
     inherit nix-std;
 
-    # INFO: I change my username from time to time,
-    # An because some NixOS options inherit from from my Home-manager options,
-    # I think it's quite reasonable to have my username declared as a variable.
+    # I change my username from time to time, An because some NixOS
+    # options inherit from from my Home-manager options, I think
+    # it's quite reasonable to have my username declared as a variable.
     user = "mxxntype";
 
-    # INFO: The name of the persistent volume, so I never mess it up.
+    # I don't store my config in /etc/nixos.
+    flakePath = "/home/${user}/Aeon";
+
+    # The name of the persistent volume, so I never mess it up.
     persist = "/persist";
 
-    # INFO: SSH (and other?..) public keys, so I also never mess them up.
-    # Also, should I make a new private key, this would make it much easier
-    # to quickly update the public ones everywhere too.
+    # SSH (and other?..) public keys, so I also never mess them up. Also,
+    # should I make a new private key, this would make it much easier to
+    # quickly update the public ones everywhere too.
     pubKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvBw3klXzVq5oTXtS061cfcGEjHWflPZNRBRg48N3w/ astrumaureus@Nox"
     ];
