8d90a6a938470730941b661950fa16cdae7ccac9
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-21 17:09:51 +0300

    feat(systems): Install `illusion` QEMU VM

--- systems/x86_64-linux/illusion/default.nix
+++ systems/x86_64-linux/illusion/default.nix
@@ -15,9 +15,9 @@
 
     # NOTE: Flattened for the installer script.
     boot.initrd.systemd = {};
-    boot.initrd.kernelModules = [ ];
-    boot.initrd.availableKernelModules = [ ];
-    boot.kernelModules = [ ];
+    boot.initrd.kernelModules = [ "dm-snapshot" ];
+    boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+    boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
     system.stateVersion = "23.11"; # WARN: Changing this might break things. Just leave it.
