2da54afaa08356a3927517400d75e4c343e95263
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-21 19:29:32 +0300

    feat(modules/nushell): Implement `ssh-agent` autostart

--- modules/home/cli/shell/nushell/default.nix
+++ modules/home/cli/shell/nushell/default.nix
@@ -33,6 +33,27 @@ with lib; {
 
                 envFile.text = /* nu */ ''
                     $env.LS_COLORS = "${config.aeon.theme.cli.ls}"
+
+                    # Autostart an SSH agent and don't start more than one of it.
+                    let ssh_agent_env_path = $"/tmp/ssh-agent-($env.USER).nuon"
+                    if ($ssh_agent_env_path | path exists) and ($"/proc/(open $ssh_agent_env_path | get SSH_AGENT_PID)" | path exists) {
+                        load-env (open $ssh_agent_env_path)
+                    } else {
+                        ^ssh-agent -c
+                            | lines
+                            | first 2
+                            | parse "setenv {name} {value};"
+                            | transpose -r
+                            | into record
+                            | save --force $ssh_agent_env_path
+                        load-env (open $ssh_agent_env_path)
+                    }
+
+                    # If no keys are added, prompt to add one ASAP.
+                    let added_keys_count = ssh-add -l | lines | enumerate | where item =~ SHA | length
+                    if $added_keys_count == 0 {
+                        ssh-add
+                    }
                 '';
             };
 
