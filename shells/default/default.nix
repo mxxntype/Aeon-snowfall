b1d307cdcb2bef01722c71dc5055d0c4c3a5a17f
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-23 15:42:28 +0300

    fix(shells): Add `cryptsetup`, `util-linux`, `efibootmgr`, `btrfs-progs`

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -6,6 +6,10 @@
 pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     buildInputs = with pkgs; [
+        cryptsetup
+        util-linux
+        efibootmgr
+        btrfs-progs
         git
         home-manager
         helix
