e2bd8e449bc39ff17f45914a1bd434968d4f3616
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-23 15:43:44 +0300

    feat(modules/boot): Add `encrypted` toggle for LUKS2 support

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -9,28 +9,35 @@
 
 with lib; {
     options.aeon.boot = {
+        # What type of bootloader module to use.
         type = mkOption {
-            type = types.enum [ "bios" "uefi" "lanzaboote" ];
+            type = with types; enum [ "bios" "uefi" "lanzaboote" ];
             default = "uefi";
-            description = "What type of bootloader module to use";
         };
 
-        quiet = mkOption {
-            type = types.bool;
+        # Whether to use LUKS2 encryption.
+        encrypted = mkOption {
+            type = with types; bool;
             default = false;
-            description = "Whether to enable Plymouth and reduce TTY verbosity";
         };
 
+        # Whether to enable Plymouth and reduce TTY verbosity.
+        quiet = mkOption {
+            type = with types; bool;
+            default = false;
+        };
+
+        # Link to boot.loader.grub.device.
         grub.device = mkOption {
-            type = types.str;
+            type = with types; str;
             default = "nodev";
-            description = "Link to boot.loader.grub.device";
         };
     };
 
     config = let
-        inherit (config.aeon.boot) type quiet;
+        inherit (config.aeon.boot) type quiet encrypted;
         inherit (config.aeon.boot.grub) device;
+        inherit (config.networking) hostName;
     in mkMerge [
         # Use GRUB2 by default.
         {
@@ -42,41 +49,27 @@ with lib; {
 
         # BIOS:
         (mkIf (type == "bios") {
-            boot = {
-                loader = {
-                    grub = {
-                        efiSupport = mkDefault false;
-                    };
-                };
-            };
+            boot.loader.grub.efiSupport = false;
         })
 
         # UEFI: common options.
         (mkIf (type == "uefi" || type == "lanzaboote") {
-            boot = {
-                loader = {
-                    efi = {
-                        efiSysMountPoint = mkDefault "/boot/efi";
-                        canTouchEfiVariables = true;
-                    };
-                };
+            boot.loader.efi = {
+                efiSysMountPoint = "/boot/efi";
+                canTouchEfiVariables = true;
             };
 
             fileSystems.${config.boot.loader.efi.efiSysMountPoint} = {
-                device = "/dev/disk/by-label/EFI";
+                device = "/dev/disk/by-label/NIXOS_EFI";
                 fsType = "vfat";
             };
         })
 
         # UEFI: GRUB2 non-secure boot.
         (mkIf (type == "uefi") {
-            boot = {
-                loader = {
-                    grub = {
-                        efiSupport = true;
-                        efiInstallAsRemovable = mkDefault false;
-                    };
-                };
+            boot.loader.grub = {
+                efiSupport = true;
+                efiInstallAsRemovable = mkDefault false;
             };
         })
 
@@ -94,6 +87,29 @@ with lib; {
                     # HACK: Lanzaboote currently replaces systemd-boot, so force it to false for now.
                     systemd-boot.enable = mkForce false;
                     grub.enable = mkForce false;
+                };
+            };
+        })
+
+        # LUKS2 support.
+        (mkIf encrypted {
+            boot = {
+                loader.grub.enableCryptodisk = true;
+                initrd = let
+                    FDE = (type != "lanzaboote");
+                    keyfile = "/keyfile-${toLower hostName}.bin";
+                in {
+                    luks.devices."root" = {
+                        device = "/dev/disk/by-label/${toUpper hostName}_LUKS";
+                        preLVM = true;
+                        allowDiscards = true;
+                        keyFile = mkIf FDE keyfile;
+                    };
+
+                    # Include necessary keyfiles in the InitRD.
+                    secrets = mkIf FDE {
+                        ${keyfile} = "/etc/secrets/initrd/keyfile-${toLower hostName}.bin";
+                    };
                 };
             };
         })
