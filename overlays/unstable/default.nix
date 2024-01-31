ea090ce684a38fd4adae744ba78f9a0ac29057aa
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-01-31 22:08:22 +0300

    feat(overlays): Add `unstable` nixpkgs overlay

---
+++ overlays/unstable/default.nix
@@ -0,0 +1,22 @@
+# INFO: "Unstable" nixpkgs overlay.
+#
+# This overlay replaces certain packages with their versions from nixpkgs/nixos-unstable,
+# which allows us to have a "stable" system with only select stuff pulled in from the
+# bleeding-edge `nixos-unstable` branch of nixpkgs. All in one overlay, because why not.
+#
+# NOTE: Channels are named after nixpkgs instances in your flake inputs. For example, with
+# the input `nixpkgs` there will be a channel available at `channels.nixpkgs`. These
+# channels are system-specific instances of nixpkgs that can be used to quickly pull
+# packages into your overlay. All other arguments for this function are flake inputs.
+
+{
+    channels,
+    ...
+}:
+
+final: prev: {
+    inherit (channels.unstable)
+        helix
+        nushell
+        ;
+}
