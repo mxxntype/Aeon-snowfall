f28e068ddce7881a6466025eef367050452a61fe
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-09 23:41:33 +0300

    refactor(packages/aeon): Use `disko` to take care of filesystems

--- packages/aeon/default.nix
+++ packages/aeon/default.nix
@@ -41,17 +41,13 @@ pkgs.nuenv.writeScriptBin {
             exec $"./result/bin/run-($system)-vm"
         }
 
-        # Perform a semi-automatic NixOS install.
+        # Perform a semi-automatic NixOS install using Disko.
         def "main install" [
             hostname: string # Future hostname of the installed system.
             platform: string = "x86_64-linux" # Target platform (architecture).
-            --partition (-p) # Partition the target drive.
-            --create-fs (-c) # Create FS on the target drive.
+            --mode: string = "mount" # The mode in which Disko runs.
             --mount (-m): directory = /mnt # Where to mount the target drive.
             --install (-I) # Run nixos-install.
-            --BIOS (-B) # Use legacy BIOS boot instead of UEFI.
-            --LUKS (-L) # Use LUKS2 encryption.
-            --root-lv-size (-R): int = 100 # Size of the root LVM LV in %.
             --ignore-generated-config (-i) # Do not automatically inherit `boot.*` options from `nixos-generate-config`.
             --copy-to: directory = /home/${lib.aeon.user} # Where to copy the repo (in /mnt).
             --no-copy-repo # Do not copy the repo to the target drive.
@@ -72,81 +68,84 @@ pkgs.nuenv.writeScriptBin {
             assert ("~/.ssh/id_ed25519" | path exists) "SSH private key not found."
             assert ("~/.config/sops/age/keys.txt" | path exists) "Age private keys not found."
 
+            # Run Disko.
+            disko --mode $mode --flake $"(realpath .)#($hostname)"
+
             # Run `fdisk` to partition a drive.
-            if $partition {
-                $target_drive = (select_blockdev --type "disk" --hint "installation drive")
-                print $"Starting (ansi blue)fdisk(ansi reset) to partition target drive..."
-                sudo fdisk $target_drive
-            }
+            # if $partition {
+            #     $target_drive = (select_blockdev --type "disk" --hint "installation drive")
+            #     print $"Starting (ansi blue)fdisk(ansi reset) to partition target drive..."
+            #     sudo fdisk $target_drive
+            # }
 
             # TODO: Implement filesystems other than BTRFS.
-            if $create_fs {
-                mut root_part: path = ""
+            # if $create_fs {
+            #     mut root_part: path = ""
                 
-                if $LUKS {
-                    print $"Setting up (ansi blue_bold)LUKS(ansi reset)..."
+            #     if $LUKS {
+            #         print $"Setting up (ansi blue_bold)LUKS(ansi reset)..."
 
-                    # Create the LUKS device.
-                    let luks_part: path = (select_blockdev --type "part" --hint "LUKS partition")
-                    let label: string = $"($hostname | str upcase)_LUKS"
-                    cryptsetup luksFormat --label $label $luks_part
+            #         # Create the LUKS device.
+            #         let luks_part: path = (select_blockdev --type "part" --hint "LUKS partition")
+            #         let label: string = $"($hostname | str upcase)_LUKS"
+            #         cryptsetup luksFormat --label $label $luks_part
 
-                    # Add a keyfile for single-passphrase boot.
-                    dd if=/dev/urandom $"of=($keyfile)" bs=1024 count=4
-                    cryptsetup luksAddKey $luks_part $keyfile
+            #         # Add a keyfile for single-passphrase boot.
+            #         dd if=/dev/urandom $"of=($keyfile)" bs=1024 count=4
+            #         cryptsetup luksAddKey $luks_part $keyfile
 
-                    # Open the LUKS device.
-                    let mapped_name: string = $"($hostname)-luks"
-                    cryptsetup luksOpen $luks_part $mapped_name -d $keyfile
-                    $root_part = $"/dev/mapper/($mapped_name)"
-                } else {
-                    # Pick a regular non-encrypted partition.
-                    $root_part = (select_blockdev --type "part" --hint "root partition")
-                }
+            #         # Open the LUKS device.
+            #         let mapped_name: string = $"($hostname)-luks"
+            #         cryptsetup luksOpen $luks_part $mapped_name -d $keyfile
+            #         $root_part = $"/dev/mapper/($mapped_name)"
+            #     } else {
+            #         # Pick a regular non-encrypted partition.
+            #         $root_part = (select_blockdev --type "part" --hint "root partition")
+            #     }
 
-                # Set up LVM.
-                print $"Setting up (ansi blue_bold)LVM(ansi reset)..."
-                sudo vgcreate $hostname $root_part
-                sudo lvcreate -n root -l $"($root_lv_size)%FREE" $hostname
-                let root_lv: path = $"/dev/($hostname)/root"
+            #     # Set up LVM.
+            #     print $"Setting up (ansi blue_bold)LVM(ansi reset)..."
+            #     sudo vgcreate $hostname $root_part
+            #     sudo lvcreate -n root -l $"($root_lv_size)%FREE" $hostname
+            #     let root_lv: path = $"/dev/($hostname)/root"
 
-                # Create BTFS.
-                print $"Creating (ansi blue_bold)BTRFS(ansi reset) filesystem and subvolumes..."
-                sudo mkfs.btrfs $root_lv -L $"($hostname | str upcase)_BTRFS" -q
-                sudo mount $root_lv $mount
+            #     # Create BTFS.
+            #     print $"Creating (ansi blue_bold)BTRFS(ansi reset) filesystem and subvolumes..."
+            #     sudo mkfs.btrfs $root_lv -L $"($hostname | str upcase)_BTRFS" -q
+            #     sudo mount $root_lv $mount
 
-                # Select & create subvolumes.
-                const subvolumes: list<string> = ["@" "@home" "@nix" "@persist"]
-                let selected = ($subvolumes | input list --multi "What BTFS subvolumes to create")
-                for subvolume in $selected {
-                    sudo btrfs subvolume create $"($mount)\/($subvolume)"
-                }
-                sudo umount -R $mount
+            #     # Select & create subvolumes.
+            #     const subvolumes: list<string> = ["@" "@home" "@nix" "@persist"]
+            #     let selected = ($subvolumes | input list --multi "What BTFS subvolumes to create")
+            #     for subvolume in $selected {
+            #         sudo btrfs subvolume create $"($mount)\/($subvolume)"
+            #     }
+            #     sudo umount -R $mount
 
-                # Mount each subvolume with options.
-                for subvolume in $selected {
-                    let subdir: path = ($subvolume | str trim -c "@")
-                    if not ($subdir | is-empty) { sudo mkdir -p $"($mount)\/($subdir)" }
-                    sudo mount $root_lv $"($mount)\/($subdir)" -o $"compress=zstd,space_cache=v2,subvol=($subvolume)"
-                }
+            #     # Mount each subvolume with options.
+            #     for subvolume in $selected {
+            #         let subdir: path = ($subvolume | str trim -c "@")
+            #         if not ($subdir | is-empty) { sudo mkdir -p $"($mount)\/($subdir)" }
+            #         sudo mount $root_lv $"($mount)\/($subdir)" -o $"compress=zstd,space_cache=v2,subvol=($subvolume)"
+            #     }
 
-                # Create a keyfile for LUKS.
-                if $LUKS {
-                    let secrets: directory = $"($mount)/etc/secrets/initrd"
-                    ^mkdir -p $secrets
-                    cp $keyfile $"($secrets)/keyfile-($hostname | str downcase).bin"
-                    chmod 000 $keyfile
-                    chattr +i $keyfile
-                }
+            #     # Create a keyfile for LUKS.
+            #     if $LUKS {
+            #         let secrets: directory = $"($mount)/etc/secrets/initrd"
+            #         ^mkdir -p $secrets
+            #         cp $keyfile $"($secrets)/keyfile-($hostname | str downcase).bin"
+            #         chmod 000 $keyfile
+            #         chattr +i $keyfile
+            #     }
 
-                # Create the UEFI partition if needed.
-                if not $BIOS {
-                    let efi_part: path = (select_blockdev --type "part" --hint "EFI partition")
-                    sudo mkfs.fat -F 32 -n NIXOS_EFI $efi_part
-                    sudo mkdir -p $"($mount)/boot/efi"
-                    sudo mount $efi_part $"($mount)/boot/efi"
-                }
-            }
+            #     # Create the UEFI partition if needed.
+            #     if not $BIOS {
+            #         let efi_part: path = (select_blockdev --type "part" --hint "EFI partition")
+            #         sudo mkfs.fat -F 32 -n NIXOS_EFI $efi_part
+            #         sudo mkdir -p $"($mount)/boot/efi"
+            #         sudo mount $efi_part $"($mount)/boot/efi"
+            #     }
+            # }
 
             # Copy needed hardware-related options from generated config.
             if not $ignore_generated_config {
@@ -233,21 +232,21 @@ pkgs.nuenv.writeScriptBin {
             }
 
             # Pick a drive/partition from the ones available.
-            def select_blockdev [
-                --type: string # `disk` or `part`
-                --hint: string # Hint of what's happening for the user.
-            ]: nothing -> string {
-                let lsblk = lsblk -JO | from json | get blockdevices
-                let devices = ($lsblk | upsert children 0 | reject children) | append ($lsblk | get -i children | flatten) | filter {|d| not ($d | is-empty)}
-                let target = $devices
-                    | where type =~ $type
-                    | select name path size model fstype
-                    | input list --fuzzy $"Choose the (ansi blue)($hint)(ansi reset)"
-                    | get path
+            # def select_blockdev [
+            #     --type: string # `disk` or `part`
+            #     --hint: string # Hint of what's happening for the user.
+            # ]: nothing -> string {
+            #     let lsblk = lsblk -JO | from json | get blockdevices
+            #     let devices = ($lsblk | upsert children 0 | reject children) | append ($lsblk | get -i children | flatten) | filter {|d| not ($d | is-empty)}
+            #     let target = $devices
+            #         | where type =~ $type
+            #         | select name path size model fstype
+            #         | input list --fuzzy $"Choose the (ansi blue)($hint)(ansi reset)"
+            #         | get path
 
-                print $"Selected (ansi blue)($hint)(ansi reset): (ansi default_underline)($target)(ansi reset)"
-                return $target
-            }
+            #     print $"Selected (ansi blue)($hint)(ansi reset): (ansi default_underline)($target)(ansi reset)"
+            #     return $target
+            # }
         }
 
         # Set a new wallpaper.
