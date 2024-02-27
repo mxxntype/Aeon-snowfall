43a0a08c56104be9a7240a6cd38680e282e2d865
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-27 23:11:23 +0300

    feat(packages): Add `smart-offload` package

---
+++ packages/smart-offload/default.nix
@@ -0,0 +1,24 @@
+# INFO: A script to Run commands on the NVIDIA GPU if possible.
+
+{
+    pkgs,
+    ...
+}:
+
+pkgs.nuenv.writeScriptBin {
+    name = "smart-offload";
+    script = /* nu */ ''
+        # Run commands on the NVIDIA GPU if possible.
+        def main --wrapped [
+            ...rest # The command to try to offload.
+        ]: nothing -> nothing {
+            let prefix: string = match (which nvidia-offload | is-empty) {
+                false => "nvidia-offload ",
+                true => ""
+            }
+            let command: string = $"($prefix)($rest | str join (char space))"
+            print $"(ansi magenta)smart-offload:(ansi reset) Running (ansi {fg: cyan, bg: black}) ($command) (ansi reset)"
+            exec ${pkgs.bash}/bin/bash -c $command
+        }
+    '';
+}
