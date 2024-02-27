af8a36a0e20861c534f13281f8fa853b3dc5402f
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-27 17:45:55 +0300

    feat(packages/aeon): Update `aeon install` for use with `disko`

--- packages/aeon/default.nix
+++ packages/aeon/default.nix
@@ -10,7 +10,16 @@ pkgs.nuenv.writeScriptBin {
     name = "aeon";
     script = /* nu */ ''
         # A Nushell script for managing and installing Aeon systems.
-        def main []: nothing -> nothing {}
+        def main []: nothing -> nothing {
+            let sysinfo = {}
+                | upsert host (sys | get host | reject sessions)
+                | upsert disks (sys | get disks | reject removable | uniq-by device)
+                | upsert net (sys | get net | sort-by recv | reverse)
+
+            print ""
+            print $sysinfo
+            print $"(ansi green)note: (ansi reset)Run (ansi {fg: cyan, bg: black}) aeon --help (ansi reset) to see more options.(char nl)"
+        }
 
         # Garbage-collect the system.
         def "main gc" []: nothing -> nothing {
@@ -52,6 +61,7 @@ pkgs.nuenv.writeScriptBin {
             --copy-to: directory = /home/${lib.aeon.user} # Where to copy the repo (in /mnt).
             --no-copy-repo # Do not copy the repo to the target drive.
             --no-copy-keys # Do not copy SSH host keys (will cause problems with sops-nix!)
+            --secure # Install assuming UEFI Secure boot will be in use.
         ]: nothing -> nothing {
             const SOPSFILE: path = "./.sops.yaml" 
             const SECRETS: path = "./lib/secrets.yaml"
@@ -69,87 +79,12 @@ pkgs.nuenv.writeScriptBin {
             assert ("~/.config/sops/age/keys.txt" | path exists) "Age private keys not found."
 
             # Run Disko.
+            $"Running ($'disko (ansi green)--mode (ansi wb)($mode)' | code cyan)" | trace
             disko --mode $mode --flake $"(realpath .)#($hostname)"
-
-            # Run `fdisk` to partition a drive.
-            # if $partition {
-            #     $target_drive = (select_blockdev --type "disk" --hint "installation drive")
-            #     print $"Starting (ansi blue)fdisk(ansi reset) to partition target drive..."
-            #     sudo fdisk $target_drive
-            # }
-
-            # TODO: Implement filesystems other than BTRFS.
-            # if $create_fs {
-            #     mut root_part: path = ""
-                
-            #     if $LUKS {
-            #         print $"Setting up (ansi blue_bold)LUKS(ansi reset)..."
-
-            #         # Create the LUKS device.
-            #         let luks_part: path = (select_blockdev --type "part" --hint "LUKS partition")
-            #         let label: string = $"($hostname | str upcase)_LUKS"
-            #         cryptsetup luksFormat --label $label $luks_part
-
-            #         # Add a keyfile for single-passphrase boot.
-            #         dd if=/dev/urandom $"of=($keyfile)" bs=1024 count=4
-            #         cryptsetup luksAddKey $luks_part $keyfile
-
-            #         # Open the LUKS device.
-            #         let mapped_name: string = $"($hostname)-luks"
-            #         cryptsetup luksOpen $luks_part $mapped_name -d $keyfile
-            #         $root_part = $"/dev/mapper/($mapped_name)"
-            #     } else {
-            #         # Pick a regular non-encrypted partition.
-            #         $root_part = (select_blockdev --type "part" --hint "root partition")
-            #     }
-
-            #     # Set up LVM.
-            #     print $"Setting up (ansi blue_bold)LVM(ansi reset)..."
-            #     sudo vgcreate $hostname $root_part
-            #     sudo lvcreate -n root -l $"($root_lv_size)%FREE" $hostname
-            #     let root_lv: path = $"/dev/($hostname)/root"
-
-            #     # Create BTFS.
-            #     print $"Creating (ansi blue_bold)BTRFS(ansi reset) filesystem and subvolumes..."
-            #     sudo mkfs.btrfs $root_lv -L $"($hostname | str upcase)_BTRFS" -q
-            #     sudo mount $root_lv $mount
-
-            #     # Select & create subvolumes.
-            #     const subvolumes: list<string> = ["@" "@home" "@nix" "@persist"]
-            #     let selected = ($subvolumes | input list --multi "What BTFS subvolumes to create")
-            #     for subvolume in $selected {
-            #         sudo btrfs subvolume create $"($mount)\/($subvolume)"
-            #     }
-            #     sudo umount -R $mount
-
-            #     # Mount each subvolume with options.
-            #     for subvolume in $selected {
-            #         let subdir: path = ($subvolume | str trim -c "@")
-            #         if not ($subdir | is-empty) { sudo mkdir -p $"($mount)\/($subdir)" }
-            #         sudo mount $root_lv $"($mount)\/($subdir)" -o $"compress=zstd,space_cache=v2,subvol=($subvolume)"
-            #     }
-
-            #     # Create a keyfile for LUKS.
-            #     if $LUKS {
-            #         let secrets: directory = $"($mount)/etc/secrets/initrd"
-            #         ^mkdir -p $secrets
-            #         cp $keyfile $"($secrets)/keyfile-($hostname | str downcase).bin"
-            #         chmod 000 $keyfile
-            #         chattr +i $keyfile
-            #     }
-
-            #     # Create the UEFI partition if needed.
-            #     if not $BIOS {
-            #         let efi_part: path = (select_blockdev --type "part" --hint "EFI partition")
-            #         sudo mkfs.fat -F 32 -n NIXOS_EFI $efi_part
-            #         sudo mkdir -p $"($mount)/boot/efi"
-            #         sudo mount $efi_part $"($mount)/boot/efi"
-            #     }
-            # }
 
             # Copy needed hardware-related options from generated config.
             if not $ignore_generated_config {
-                print $"Copying options from (ansi blue_bold)nixos-generate-config(ansi reset)..."
+                $"Copying options from ($'nixos-generate-config' | code blue)" | trace
                 let options = sudo nixos-generate-config --root $mount --show-hardware-config
                     | lines
                     | filter {|l| ($l | str downcase) =~ "boot"}
@@ -164,7 +99,7 @@ pkgs.nuenv.writeScriptBin {
 
             # Generate keys (so that sops-nix works fine).
             if not $no_copy_keys {
-                print $"Generating host (ansi blue_bold)SSH(ansi reset) keys..."
+                $"Generating host ('SSH' | code blue) keys..." | trace
 
                 let tmpdir = mktemp -d
                 sudo mkdir -p $"($tmpdir)/etc/ssh"
@@ -177,7 +112,7 @@ pkgs.nuenv.writeScriptBin {
                     sudo cp $"($tmpdir)/etc/ssh/ssh_host_($type)_key*" $"($mount)/etc/ssh/"
                 }
 
-                print $"Re-keying (ansi blue_bold)sops-nix(ansi reset)..."
+                $"Re-keying ('sops-nix' | code yellow)" | trace
 
                 # The new host's key (should stay there).
                 let age_pubkey = open $"($tmpdir)/etc/ssh/ssh_host_ed25519_key.pub" | ssh-to-age;
@@ -194,15 +129,23 @@ pkgs.nuenv.writeScriptBin {
                 sudo chown root:root $"($mount)/etc/ssh/ssh_host_*_key*"
             }
 
+            if $secure {
+                $"Creating UEFI Secure boot keys with ('sbctl' | code)" | trace
+                do -ps {
+                    sudo sbctl create-keys
+                    sudo cp -r /etc/secureboot /mnt/etc/
+                }
+            }
+
             # Run `nixos-install`.
             if $install {
-                print $"Running (ansi red)nixos-install(ansi reset)..."
+                $"Running ('nixos-install' | code red)" | trace
                 sudo nixos-install --no-root-password --root $mount --flake $".#($hostname)"
             }
 
             # WARN: Remove installer ISO's SSH key from sops-nix (if it was added).
             if not ($sopsfile | is-empty) {
-                print $"(ansi red)Removing(ansi reset) installer ISO's SSH key from (ansi blue_bold)sops-nix(ansi reset)..."
+                $"Removing installer ISO's keys from ($SOPSFILE | path basename | code green)" | trace
                 $sopsfile | save -rf $SOPSFILE
             }
 
@@ -215,6 +158,11 @@ pkgs.nuenv.writeScriptBin {
                 cd $repo
             }
 
+            $"('aeon install' | code) is finished. Please check for any errors."                | trace
+            $"If everything is alright, run ('sync' | code) and ($'umount -r ($mount)' | code)" | trace
+            $"Then, you should be ready to reboot into the installed system."                   | trace
+            $"(ansi green_bold)Good luck!(ansi reset)"                                          | trace
+
             # Update .sops.yaml with new host key.
             def add_sops_host_key [
                 --key: string # Age public key to add.
@@ -223,7 +171,7 @@ pkgs.nuenv.writeScriptBin {
                 if (${pkgs.ripgrep}/bin/rg $host $SOPSFILE | is-empty ) {
                     # Add a new key.
                     ${pkgs.sd}/bin/sd $ANCHOR $"\n  - &($host) ($key)($ANCHOR)" $SOPSFILE
-                    echo $"      - *($host)\n" | save --append $SOPSFILE
+                    $"      - *($host)\n" | save --append $SOPSFILE
                 } else {
                     # Update an existing key.
                     ${pkgs.sd}/bin/sd $'&($host) \w*$' $'&($host) ($key)' $SOPSFILE
@@ -231,22 +179,18 @@ pkgs.nuenv.writeScriptBin {
                 sops updatekeys $SECRETS
             }
 
-            # Pick a drive/partition from the ones available.
-            # def select_blockdev [
-            #     --type: string # `disk` or `part`
-            #     --hint: string # Hint of what's happening for the user.
-            # ]: nothing -> string {
-            #     let lsblk = lsblk -JO | from json | get blockdevices
-            #     let devices = ($lsblk | upsert children 0 | reject children) | append ($lsblk | get -i children | flatten) | filter {|d| not ($d | is-empty)}
-            #     let target = $devices
-            #         | where type =~ $type
-            #         | select name path size model fstype
-            #         | input list --fuzzy $"Choose the (ansi blue)($hint)(ansi reset)"
-            #         | get path
+            # Print a status message.
+            def trace []: string -> nothing {
+                print $"(ansi {fg: magenta, attr: b})aeon:(ansi reset) ($in)"
+            }
 
-            #     print $"Selected (ansi blue)($hint)(ansi reset): (ansi default_underline)($target)(ansi reset)"
-            #     return $target
-            # }
+            # Make a markdown-like code block.
+            def code [
+                fg: string = "cyan",
+                attr: string = "n",
+            ]: string -> string {
+                $"(ansi {fg: $fg, bg: black, attr: $attr}) ($in) (ansi reset)"
+            }
         }
 
         # Set a new wallpaper.
