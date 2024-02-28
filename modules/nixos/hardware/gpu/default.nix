3a90507f8056f4f03b177e6148323af12782907b
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-28 16:16:16 +0300

    feat(modules/gpu): Implement `iGPU` / `dGPU` specialisations

--- modules/nixos/hardware/gpu/default.nix
+++ modules/nixos/hardware/gpu/default.nix
@@ -8,8 +8,8 @@
 }:
 
 with lib; {
-    options.aeon.gpu = {
-        core = mkOption {
+    options.aeon.hardware.gpu = {
+        core.enable = mkOption {
             description = "Whether to add common GPU-related modules";
             type = with types; bool;
             default = true;
@@ -40,16 +40,39 @@ with lib; {
                 default = null;
             };
         };
+
+        specialise = mkOption {
+            description = "Whether to split iGPU/dGPU specialisations";
+            type = with types; bool;
+            default = false;
+        };
     };
 
     config = let
-        inherit (config.aeon.gpu)
+        nvidiaConfig = {
+            services.xserver.videoDrivers = mkBefore [ "nvidia" ];
+            hardware.nvidia = {
+                package = config.boot.kernelPackages.nvidiaPackages.stable;
+                modesetting.enable = true;
+                powerManagement.enable = true;
+                prime = {
+                    intelBusId = intel.busID;
+                    nvidiaBusId = nvidia.busID;
+                    offload = {
+                        enable = true;
+                        enableOffloadCmd = true;
+                    };
+                };
+            };
+        };
+        inherit (config.aeon.hardware.gpu)
             core
             intel
             nvidia
+            specialise
             ;
     in mkMerge [
-        (mkIf core {
+        (mkIf core.enable {
             # Exclude `nvtop` from minimal systems.
             environment.systemPackages = with pkgs; [ aeon.smart-offload ] ++
                 (if (config.networking.hostName != "illusion")
@@ -74,21 +97,50 @@ with lib; {
             };
         })
 
-        (mkIf nvidia.enable {
-            services.xserver.videoDrivers = [ "nvidia" ];
-            hardware.nvidia = {
-                package = config.boot.kernelPackages.nvidiaPackages.stable;
-                modesetting.enable = true;
-                powerManagement.enable = true;
-                prime = {
-                    intelBusId = intel.busID;
-                    nvidiaBusId = nvidia.busID;
-                    offload = {
-                        enable = true;
-                        enableOffloadCmd = true;
-                    };
+        (mkIf (nvidia.enable && !specialise) nvidiaConfig)
+
+        (mkIf (nvidia.enable && specialise) (mkMerge [
+            # Create a dGPU spec with the necessary drivers.
+            {
+                specialisation."dGPU".configuration = mkMerge [
+                    {
+                        system.nixos.label = "${config.networking.hostName}-dGPU";
+                    }
+                    nvidiaConfig
+                ];
+            }
+
+            # Disable the NVIDIA dGPU in the default specialisation.
+            #
+            # NOTE: Stolen from https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/nvidia/disable.nix
+            # Tried to use an import, but conditional imports are a f**king nightmare, so just hardcode for now (forever).
+            (mkIf (config.specialisation != { }) {
+                boot = {
+                    blacklistedKernelModules = [
+                        "nouveau"
+                        "nvidia"
+                        "nvidia_drm"
+                        "nvidia_modeset"
+                    ];
+
+                    extraModprobeConfig = ''
+                        blacklist nouveau
+                        options nouveau modeset=0
+                    '';
                 };
-            };
-        })
+    
+                system.nixos.label = "${config.networking.hostName}-iGPU";
+                services.udev.extraRules = /* python */ ''
+                    # Remove NVIDIA USB xHCI Host Controller devices, if present.
+                    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+                    # Remove NVIDIA USB Type-C UCSI devices, if present.
+                    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+                    # Remove NVIDIA Audio devices, if present.
+                    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+                    # Remove NVIDIA VGA/3D controller devices.
+                    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+                '';
+            })
+        ]))
     ];
 }
