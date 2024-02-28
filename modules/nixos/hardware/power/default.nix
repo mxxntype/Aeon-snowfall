90e2a83488495bf1b248fa5fd6144633075709cb
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-28 10:00:50 +0300

    refactor(modules/power): Move `powerdrain` script to own package

--- modules/nixos/hardware/power/default.nix
+++ modules/nixos/hardware/power/default.nix
@@ -10,21 +10,28 @@
 with lib; {
     options.aeon.powerManagement = {
         type = mkOption {
-            type = types.enum [ "laptop" "desktop" ];
-            default = "desktop";
             description = "What style of power-management to use";
+            type = with types; enum [ "laptop" "desktop" ];
+            default = "desktop";
         };
 
         service = mkOption {
-            type = types.enum [ "auto-cpufreq" ];
-            default = "auto-cpufreq";
             description = "What service to use for power-management";
+            type = with types; enum [ "auto-cpufreq" ];
+            default = "auto-cpufreq";
         };
     };
 
     config = let
-        inherit (config.aeon.powerManagement) type service;
+        inherit (config.aeon.powerManagement)
+            type
+            service
+            ;
     in mkMerge [
+        {
+            environment.systemPackages = with pkgs; [ aeon.powerdrain ];
+        }
+
         (mkIf (service == "auto-cpufreq") {
             services.auto-cpufreq = if (type == "laptop") then {
                 enable = true;
@@ -38,24 +45,9 @@ with lib; {
                         turbo = "auto";
                     };
                 };
-            } else {};
+            } else {
+                # TODO: Desktop profiles.
+            };
         })
-
-        {
-            environment.systemPackages = [(pkgs.nuenv.writeScriptBin {
-                name = "powerdrain";
-                script = /* nu */ ''
-                    const bat: string = "/sys/class/power_supply/BAT0"
-                    const K: int = 1000000000000
-
-                    if ($bat | path exists) {
-                        let current: int = (open $"($bat)/current_now" | into int)
-                        let voltage: int = (open $"($bat)/voltage_now" | into int)
-                        let power: int = ($current * $voltage / $K)
-                        $power | into int
-                    } else { "N/A" }
-                '';
-            })];
-        }
     ];
 }
---
+++ packages/powerdrain/default.nix
@@ -0,0 +1,21 @@
+# INFO: A script to get the current wattage of the system.
+
+{
+    pkgs,
+    ...
+}:
+
+pkgs.nuenv.writeScriptBin {
+    name = "powerdrain";
+    script = /* nu */ ''
+        const bat: string = "/sys/class/power_supply/BAT0"
+        const K: int = 1000000000000
+
+        if ($bat | path exists) {
+            let current: int = (open $"($bat)/current_now" | into int)
+            let voltage: int = (open $"($bat)/voltage_now" | into int)
+            let power: int = ($current * $voltage / $K)
+            $power | into int
+        } else { "N/A" }
+    '';
+}
