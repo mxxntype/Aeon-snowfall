326167cf971e785c5689d32fa8c5c621003fe4b5
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-03-05 14:55:25 +0300

    feat(shells): Add `rust-gui` shell

---
+++ shells/rust-gui/default.nix
@@ -0,0 +1,41 @@
+# INFO: A shell for running Rust GUIs on NixOS.
+#
+# No GPU / GUI / Gamedev Rust-powered libraries and engines that I've tried worked
+# on NixOS, neither with X11 nor Wayland. However, it seems to work in this shell.
+# SOURCE: https://github.com/bevyengine/bevy/issues/9203#issuecomment-1657248743
+#
+# TODO: Maybe provide Rust and Cargo from one of the flake interfaces?
+
+{
+    pkgs,
+    lib,
+    ...
+}:
+
+pkgs.mkShell rec {
+    nativeBuildInputs = with pkgs; [
+        pkg-config
+        cargo
+        rustc
+        rustfmt
+    ];
+
+    buildInputs = with pkgs; [
+        # Common.
+        udev
+        alsa-lib
+        vulkan-loader
+        # X11.
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        # Wayland.
+        libxkbcommon
+        wayland
+
+    ];
+
+    LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
