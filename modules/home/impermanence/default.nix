847c94c91870bcf55ca56e34ddbffc6d47b23868
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-03-18 10:51:08 +0300

    feat(modules/home): Add `impermanence` Home-manager module

---
+++ modules/home/impermanence/default.nix
@@ -0,0 +1,43 @@
+{
+    inputs,
+    lib,
+    config,
+    ...
+}:
+
+with lib; {
+    imports = with inputs; [
+        impermanence.nixosModules.home-manager.impermanence
+    ];
+
+    options.aeon.impermanence = {
+        enable = mkOption {
+            type = with types; bool;
+            default = true;
+            description = "Whether to apply common configurations to Impermanence";
+        };
+    };
+
+    config = let
+        inherit (config.aeon.impermanence)
+            enable
+            ;
+    in mkIf enable {
+        home.persistence."${lib.aeon.persist}/home" = {
+            directories = let
+                xdgDirs = builtins.map
+                    (dir: builtins.replaceStrings [ "${config.home.homeDirectory}/" ] [ "" ] dir)
+                    (builtins.filter
+                        (value: builtins.isString value)
+                        (builtins.attrValues config.xdg.userDirs));
+            in xdgDirs ++ [
+                ".gnupg"
+                ".ssh"
+                ".local/share/keyrings"
+                ".local/share/direnv"
+            ];
+            files = [ ".wallpaper" ];
+            allowOther = true;
+        };
+    };
+}
