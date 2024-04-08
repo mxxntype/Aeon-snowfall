69790296c21efdc2506deda68b5fab4bf44e0f8e
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-04-08 12:02:06 +0300

    feat(systems): Add `luna`

---
+++ homes/x86_64-linux/mxxntype@luna/default.nix
@@ -0,0 +1,14 @@
+{
+    ...
+}:
+
+{
+    aeon = {
+        style.codename = "overgrowth";
+        desktop.hyprland.enable = true;
+        games.minecraft.enable = true;
+        apps.discord.enable = true;
+    };
+
+    home.stateVersion = "23.11"; # WARN: Changing this might break things. Just leave it.
+}
