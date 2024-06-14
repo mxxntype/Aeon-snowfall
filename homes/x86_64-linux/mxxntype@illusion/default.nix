2516fa52389a633503c049ebc7102024ac7881fb
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-06-14 23:15:16 +0300

    chore(mxxntype@illusion): Enable `aeon.desktop.hyprland`

--- homes/x86_64-linux/mxxntype@illusion/default.nix
+++ homes/x86_64-linux/mxxntype@illusion/default.nix
@@ -6,6 +6,7 @@
     aeon = {
         style.codename = "pastel";
         cli.fzf.enable = false;
+        desktop.hyprland.enable = true;
     };
 
     home.stateVersion = "23.11"; # WARN: Changing this might break things. Just leave it.
