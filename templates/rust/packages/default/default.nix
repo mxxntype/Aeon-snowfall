5a04d122ac8e1e98df9a7f5f1ba9c6aa327ceab8
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-10-18 18:02:22 +0300

    chore(templates/rust): Bump Rust toolchain version to `1.82.0`

--- templates/rust/packages/default/default.nix
+++ templates/rust/packages/default/default.nix
@@ -8,7 +8,7 @@ let
     toolchain = (pkgs.rustChannelOf {
         rustToolchain = ../../rust-toolchain.toml;
         # NOTE: This needs to be replaced whenever the Rust toolchain is updated.
-        sha256 = "VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
+        sha256 = "yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
     }).rust;
 
     naersk' = pkgs.callPackage inputs.naersk {
