5fb2735284ca495a7f7a33ae032b7f06dd0c71e3
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-10-31 18:48:18 +0300

    feat(modules/starship): Further expand config, tweak icons

--- modules/home/cli/shell/starship/default.nix
+++ modules/home/cli/shell/starship/default.nix
@@ -30,25 +30,30 @@ with lib;
             settings = let
                 # NOTE: Shortcut for adding dark-gray square brackets around a block.
                 mkContainer = contents: "[\\[](fg:#${ui.bg.surface2})${contents}[\\]](fg:#${ui.bg.surface2}) ";
+                mkContainerRight = contents: "[\\[](fg:#${ui.bg.surface2})${contents}[\\]](fg:#${ui.bg.surface2})";
             in {
                 add_newline = false;
 
                 username = {
                     style_user = "bold purple";
                     style_root = "bold red";
-                    aliases = { "${lib.aeon.user}" = "󰼁"; };
-                    format = " [$user]($style) ";
+                    format = "[$user]($style) ";
                 };
 
                 hostname = {
                     ssh_only = true;
                     ssh_symbol = "󰑔 ";
                     style = "bold blue";
-                    format = mkContainer "(fg:white)[$ssh_symbol$hostname]($style)";
+                    format = "[$ssh_symbol]($style)${mkContainer "[$hostname]($style)"}";
                 };
 
                 directory = {
                     format = "[󰉋 $path]($style)[$read_only]($read_only_style) ";
+                    truncation_length = 6;
+                    style = "fg:#${ui.fg.subtext1}";
+                    repo_root_style = "bold white";
+                    read_only = "RO";
+                    read_only_style = "bold fg:#${colors.yellow}";
                 };
 
                 character = let style = "bold fg:#${ui.fg.subtext1}"; in {
@@ -62,17 +67,124 @@ with lib;
                 };
 
                 rust = {
-                    format = mkContainer "[$symbol$numver ($toolchain)]($style)";
+                    format = mkContainer "[$symbol $numver]($style)";
                     symbol = "󱘗";
                     style = "fg:#${colors.peach}";
+                };
+
+                package = {
+                    format = mkContainer "[$symbol$version]($style)";
+                    style = "fg:#${colors.flamingo}";
+                };
+
+                status = {
+                    disabled = false;
+                    format = "[$status]($style) ";
+                };
+
+                time = {
+                    format = mkContainerRight "[󰅐 $time]($style)";
+                    time_format = "%H:%M %p";
+                    style = "fg:#${ui.fg.subtext1}";
+                    disabled = false;
+                };
+
+                cmd_duration = {
+                    format = "[󰓅 $duration]($style) ";
+                    style = "fg:#${ui.bg.overlay1}";
+                };
+
+                fill = {
+                    symbol = " ";
                 };
 
                 # PERF: This does not affect the nix3 shell (`nix shell`),
                 # however noticeably slows down the whole environment.
                 nix_shell.disabled = true;
 
-                cmd_duration.disabled = true;
                 git_status.disabled = true;
+
+                aws.symbol = "󰸏 ";
+                buf.symbol = " ";
+                c.symbol = " ";
+                conda.symbol = "󰕗 ";
+                crystal.symbol = " ";
+                dart.symbol = " ";
+                docker_context.symbol = "󰡨 ";
+                elixir.symbol = " ";
+                elm.symbol = " ";
+                fennel.symbol = " ";
+                fossil_branch.symbol = "󰘬 ";
+                golang.symbol = "󰟓 ";
+                gradle.symbol = " ";
+                guix_shell.symbol = " ";
+                haskell.symbol = "󰲒 ";
+                haxe.symbol = " ";
+                hg_branch.symbol = "󰘬 ";
+                java.symbol = "󰬷 ";
+                julia.symbol = " ";
+                kotlin.symbol = "󱈙 ";
+                lua.symbol = "󰢱 ";
+                memory_usage.symbol = " ";
+                meson.symbol = "󰔷 ";
+                nim.symbol = " ";
+                nix_shell.symbol = "󱄅 ";
+                nodejs.symbol = "󰎙 ";
+                ocaml.symbol = " ";
+                package.symbol = "󰏗 ";
+                perl.symbol = " ";
+                php.symbol = "󰌟 ";
+                pijul_channel.symbol = "󰘬 ";
+                python.symbol = "󰌠 ";
+                rlang.symbol = "󰟔 ";
+                ruby.symbol = "󰴭 ";
+                scala.symbol = " ";
+                swift.symbol = "󰛥 ";
+                zig.symbol = " ";
+                os.symbols = {
+                    AlmaLinux = " ";
+                    Alpaquita = "󰂚 ";
+                    Alpine = " ";
+                    Amazon = " ";
+                    Android = "󰀲 ";
+                    Arch = "󰣇 ";
+                    Artix = " ";
+                    CentOS = " ";
+                    Debian = "󰣚 ";
+                    DragonFly = "󱖉 ";
+                    Emscripten = " ";
+                    EndeavourOS = " ";
+                    Fedora = "󰣛 ";
+                    FreeBSD = "󰣠 ";
+                    Garuda = " ";
+                    Gentoo = "󰣨 ";
+                    HardenedBSD = "󰞌 ";
+                    Illumos = " ";
+                    Kali = " ";
+                    Linux = "󰌽 ";
+                    Mabox = "󰏗 ";
+                    Macos = "󰀵 ";
+                    Manjaro = "󱘊 ";
+                    Mariner = "󰒸 ";
+                    MidnightBSD = "󰽥 ";
+                    Mint = "󰣭 ";
+                    NetBSD = "󰈻 ";
+                    NixOS = "󱄅 ";
+                    OpenBSD = " ";
+                    OracleLinux = "󰌷 ";
+                    Pop = " ";
+                    Raspbian = " ";
+                    RedHatEnterprise = "󰮤 ";
+                    Redhat = "󰮤 ";
+                    Redox = "󰹻 ";
+                    RockyLinux = " ";
+                    SUSE = " ";
+                    Solus = " ";
+                    Ubuntu = " ";
+                    Unknown = "󰌽 ";
+                    Void = " ";
+                    Windows = "󰖳 ";
+                };
 
                 format = builtins.replaceStrings [ "\n" ] [ "" ] ''
                     $hostname
@@ -151,14 +263,16 @@ with lib;
                     $direnv
                     $env_var
                     $crystal
+                    $package
                     $custom
                     $sudo
+                    $fill
+                    $status
                     $cmd_duration
+                    $time
                     $line_break
                     $jobs
                     $battery
-                    $time
-                    $status
                     $os
                     $container
                     $shell
