c592cd47921f0231a6ec218a611fd24666f49c57
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-06-11 19:03:58 +0300

    fix(modules/qemu): Add `libguestfs`, trust `virbr0` by default

--- modules/nixos/qemu/default.nix
+++ modules/nixos/qemu/default.nix
@@ -1,13 +1,6 @@
-# INFO: NixOS QEMU/KVM module.
+{ config, lib, pkgs, ... }: with lib;
 
 {
-    config,
-    lib,
-    pkgs,
-    ...
-}:
-
-with lib; {
     options.aeon.qemu = {
         enable = mkOption {
             description = "Whether to enable QEMU/KVM virtualisation";
@@ -18,9 +11,17 @@ with lib; {
 
     config = mkIf config.aeon.qemu.enable {
         virtualisation.libvirtd.enable = true;
+
         environment.systemPackages = with pkgs; [
+            libguestfs # NOTE: Tools for accessing and modifying virtual machine disk images.
             qemu
             virt-manager
         ];
+
+        # HACK: Not sure why, but without this setting, QEMU VMs can't seem to
+        # get an IP via DHCP, and I guess can't reach the host networking stack
+        # at all for that matter. This fixes it; I have not encountered this
+        # issue before on older NixOS versions, perhaps NFtables is to blame.
+        networking.firewall.trustedInterfaces = [ "virbr0" ];
     };
 }
