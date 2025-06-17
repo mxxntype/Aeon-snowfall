bf852aeb98d3152d66e78950645373c6b5648230
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-06-17 19:15:30 +0300

    fix(modules/syncthing): Set `"STNODEFAULTFOLDER=true"`

--- modules/home/services/syncthing/default.nix
+++ modules/home/services/syncthing/default.nix
@@ -19,5 +19,10 @@ with lib; {
         services.syncthing = {
             enable = true;
         };
+
+        # INFO: Syncthing creates a `Sync` folder in your home directory when it
+        # regenerates a configuration, even if your declarative configuration
+        # does not have this folder. Lets fix that.
+        systemd.user.services.syncthing.Service.Environment = [ "STNODEFAULTFOLDER=true" ];
     };
 }
