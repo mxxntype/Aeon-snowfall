fd6df1e3dfc4c0dad9ceb8500d9496c582ce4e6c
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-08-31 16:12:47 +0300

    chore(homes/wyrm): Remove desktop configuration

--- homes/x86_64-linux/mxxntype@wyrm/default.nix
+++ homes/x86_64-linux/mxxntype@wyrm/default.nix
@@ -2,36 +2,7 @@
 
 {
     aeon = {
-        style.codename = "kanagawa-dragon";
-
-        desktop = {
-            hyprland = {
-                enable = true;
-                source = "nixpkgs";
-            };
-
-            quickshell = { enable = true; };
-        };
-
-        monitors = [
-            {
-                name = "KTC H27P22S";  
-                port = "DP-2";
-                width = 3840;
-                height = 2160;
-                refreshRate = 144;
-                scale = 2.0;
-                offsetX = 0;
-                offsetY = 0;
-                showBar = true;
-                workspaces = [ 1 2 3 4 5 6 7 8 9 10 ];
-            }
-        ];
-
-        apps.alacritty.enable = true;
-
-        music.enable = true;
-        scream.enable = true;
+        style.codename = "kanagawa-wave";
     };
 
     # WARN: Changing this might break things. Just leave it.
