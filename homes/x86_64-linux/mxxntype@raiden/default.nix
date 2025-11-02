9925f135124b2e62c7c201188c9e98da674b1f1c
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-11-02 12:31:49 +0300

    feat(homes/mxxntype@raiden): Some more namecard wallpaper presets

--- homes/x86_64-linux/mxxntype@raiden/default.nix
+++ homes/x86_64-linux/mxxntype@raiden/default.nix
@@ -49,13 +49,17 @@
         source-image = pkgs.fetchurl {
             # url = "https://static.wikia.nocookie.net/gensin-impact/images/9/9d/Namecard_Background_Achievement_Infinitum.png/revision/latest?cb=20230412034747";
             # hash = "sha256-jMQxPdsQ9yBLU7VQ6pQ7nIuwcFe5X+8KKkUQ2GW1CEg=";
-            url = "https://static.wikia.nocookie.net/gensin-impact/images/f/f8/Namecard_Background_Travel_Notes_Woodlands.png/revision/latest?cb=20220826151244";
-            hash = "sha256-gkPwxr7lptBqn2+n8KxbPoQt3k3oxB6B3/VyMm1UDxE=";
+            # url = "https://static.wikia.nocookie.net/gensin-impact/images/f/f8/Namecard_Background_Travel_Notes_Woodlands.png/revision/latest?cb=20220826151244";
+            # hash = "sha256-gkPwxr7lptBqn2+n8KxbPoQt3k3oxB6B3/VyMm1UDxE=";
+            # url = "https://static.wikia.nocookie.net/gensin-impact/images/1/17/Namecard_Background_Inazuma_Kujou_Insignia.png/revision/latest?cb=20210725071326";
+            # hash = "sha256-PgLuAV4Ne/6GpaiMdF40lBaSm7N9K63RLuTOiamkRE4=";
+            url = "https://static.wikia.nocookie.net/gensin-impact/images/4/4c/Namecard_Background_Inazuma_Eagleplume.png/revision/latest?cb=20211013104446";
+            hash = "sha256-R+5tGwnYXogq0rzrhf0IuO9KMyXWyRap1NCTzE+8mW0=";
         };
         derivation = lib.aeon.fromNamecard {
             inherit pkgs source-image;
             border-colors = { inner = ui.bg.surface2; outer = ui.bg.base; };
-            gradient-colors = { start = ui.bg.crust; end = colors.teal; };
+            gradient-colors = { start = ui.bg.crust; end = colors.blue; };
         };
     in "${derivation}/output.png";
 }
