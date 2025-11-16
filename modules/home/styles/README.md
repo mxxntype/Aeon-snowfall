049148c2fc72cb269bc5bab2dcca501f52a2248c
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-11-16 15:32:09 +0300

    chore: Cleanup old unused styles & drop in-tree wallpapers

--- modules/home/styles/README.md
+++
@@ -1,6 +0,0 @@
-### Styles 🌺
-
-Each "rice" - I hate the word, that's why `styles/`, not `rices/` - I decide to do lives in a directory here in a directory with a codename. All of the configuration that the rice should do lives in modules in that subdir. Those option definitions only apply themselves when the `aeon.style.codename` Home-manager option matches, well, the codename. The stuff that should be common between styles does not live here, it lives in its own modules a few directories up from here. These are for different colors, WM layouts, widget styles, etc.
-
-> [!NOTE]
-> This place is kind of the reason I decided to do a full-blown rewrite. This is a raw and unfinished idea, I may or may not completely rework this. Well, as always...
--- modules/home/styles/default.nix
+++ modules/home/styles/default.nix
@@ -1,37 +1,21 @@
-# INFO: Home-manager styling module.
-
-{
-    lib,
-    config,
-    ...
-}:
+{ lib, ... }:
 
 with builtins;
 with lib; 
 
 let
-    # Automatically detect possible styles.
-    directoryContents = readDir ./.;
-    possibleCodenames = directoryContents
-        |> attrNames
-        |> filter (f: directoryContents."${f}" == "directory");
 
-    # Wallpapers found in styles' subdirs.
-    wallpapers = {
-        byCodename = { codename }: (readDir ./${codename}/wallpapers)
-            |> attrNames
-            |> map (basename: "${codename}/wallpapers/${basename}");
-        all = possibleCodenames
-            |> map (codename: wallpapers.byCodename { inherit codename; })
-            |> flatten;
-    };
-in
+# Automatically detect possible styles.
+directoryContents = readDir ./.;
+possibleCodenames = directoryContents
+    |> attrNames
+    |> filter (f: directoryContents."${f}" == "directory");
 
-{
+in {
     options.aeon.style = {
-        wm = mkOption {
-            type = with types; attrs;
-            default = { };
+        codename = mkOption {
+            type = with types; nullOr (enum possibleCodenames);
+            default = null;
         };
 
         themeFallbacks = {
@@ -40,23 +24,5 @@ in
                 default = null;
             };
         };
-
-        codename = mkOption {
-            type = with types; nullOr (enum possibleCodenames);
-            default = null;
-        };
-    };
-
-    config = let
-        inherit (config.aeon.style) codename;
-    in mkIf (codename != null) {
-        xdg.configFile = mkMerge [
-            (wallpapers.byCodename { inherit codename; }
-                |> map (wp: { name = "wallpapers/current/${wp}"; value.source = ./${wp}; })
-                |> listToAttrs)
-            (wallpapers.all
-                |> map (wp: { name = "wallpapers/${wp}"; value.source = ./${wp}; })
-                |> listToAttrs)
-        ];
     };
 }
