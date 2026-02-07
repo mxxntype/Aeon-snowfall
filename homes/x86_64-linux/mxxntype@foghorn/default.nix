78ede7042dba73ee0f5fb4f3833c4280363bca1e
mxxntype <59417007+mxxntype@users.noreply.github.com>
2026-02-07 18:47:48 +0300

    chore(systems): Prepare for `foghorn` installation

---
+++ homes/x86_64-linux/mxxntype@foghorn/default.nix
@@ -0,0 +1,43 @@
+_: {
+    aeon = {
+        style.codename = "dark-plus";
+
+        wallpapers.enable = true;
+        desktop.hyprland = {
+            enable = true;
+            source = "nixpkgs";
+        };
+        
+        monitors.monitors = [
+            {
+                name = "Integrated display";  
+                port = "eDP-1";
+                width = 2880;
+                height = 1800;
+                refreshRate = 120;
+                scale = 1.5;
+                offsetX = 0;
+                offsetY = 0;
+                showBar = true;
+                workspaces = [ 1 2 3 4 5 6 7 8 9 10 ];
+            }
+        ];
+
+        apps = {
+            wezterm.enable = true;
+            gimp.enable = true;
+            librewolf.enable = true;
+            # freetube.enable = true;
+        };
+
+        music.enable = true;
+        # scream.enable = true;
+        stylesheets.enable = true;
+    };
+
+    home = {
+        # WARN: Changing this might break things. Just leave it.
+        # The sole legit reason to change this is a reinstallation.
+        stateVersion = "25.11";
+    };
+}
---
+++ systems/x86_64-linux/foghorn/default.nix
@@ -0,0 +1,164 @@
+{ config, lib, pkgs, ... }:
+
+{
+    aeon = {
+        hardware = {
+            meta.headless = false;
+
+            cpu.type = "intel";
+            gpu = {
+                intel =  { enable = true; busID = "PCI:0:2:0"; };
+                nvidia = { enable = true; busID = "PCI:1:0:0"; };
+                specialise = true;
+            };
+
+            # vfio = {
+            #     enable = true;
+            #     specialize = true;
+            #     pciIDs = [ "10de:25ab" ];
+            # };
+
+            cups = {
+                enable = true;
+                drivers = [ pkgs.hplipWithPlugin ];
+                client = true;
+            };
+            
+            adb.enable = true;
+        };
+
+        boot.type = "uefi";
+        fs = { type = "zfs"; cacheLimitGiB = 8; };
+
+        net = {
+            ssh.server = true;
+            tailscale.ACLtags = [ "client" ];
+            wireguard.interfaces = {
+                personal.enable = true;
+                invian.enable = true;
+            };
+        };
+
+        services = {
+            # invidious.enable = true;
+            prometheus.enable = true;
+        };
+
+        sound.enable = true;
+        docker.enable = true;
+        qemu.enable = true;
+    };
+
+    disko.devices = let inherit (config.networking) hostName; in {
+        disk."system-nvme-ssd" = {
+            type = "disk";
+            device = "/dev/disk/by-id/nvme-Micron_2450_MTFDKBA1T0TFK_232140BFC1B2";
+            content = {
+                type = "gpt";
+                partitions = {
+                    ESP = {
+                        priority = 1;
+                        name = "NIXOS_EFI";
+                        size = "512M";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "defaults" ];
+                        };
+                    };
+
+                    luks = {
+                        priority = 2;
+                        size = "100%";
+                        content = {
+                            type = "luks";
+                            name = "encrypted";
+                            settings.allowDiscards = true;
+                            content = {
+                                type = "zfs";
+                                pool = "${hostName}-rpool";
+                            };
+                        };
+                    };
+                };
+            };
+        };
+       
+        zpool."${hostName}-rpool" = {
+            type = "zpool";
+            mode = "";         # No mirroring, no RAID.
+            mountpoint = null; # Use the dedicated root dataset (see below).
+
+            # NOTE: These are ZPOOL-level properties, see man zpoolprops for more.
+            # Since the underlying VDEV is a modern NVMe SSD, TRIM is good for it.
+            options.autotrim = "on";
+
+            rootFsOptions = {
+                mountpoint = "none";
+                acltype = "posixacl";
+                atime = "off";
+                compression = "zstd";
+                "com.sun:auto-snapshot" = "false";
+            };
+
+            datasets = let
+                # INFO: A small shortcut for creating legacy-mounted
+                # datasets without repeating yourself like 10 times.
+                legacy = dataset: (lib.recursiveUpdate dataset {
+                    type = "zfs_fs";
+                    options.mountpoint = "legacy";
+                });
+
+                # WARN: Though datasets can be (and some are) defined here,
+                # within Nix, ZFS has its own mounting subsystem, which makes
+                # more datasets and ZVOLs can be created later, without any
+                # modifications to this section. Stuff that I consider vital
+                # configuration is listed here, stuff that I consider state
+                # (like rootfs ZVOLs for VMs) are created outside of Nix.
+            in {
+                root = legacy { mountpoint = "/"; };
+                nix = legacy { mountpoint = "/nix"; };
+
+                "var/cache" = legacy {
+                    mountpoint = "/var/cache";
+                    options.compression = "off";
+                };
+
+                "var/log" = legacy {
+                    mountpoint = "/var/log";
+                    options.compression = "lz4";
+                };
+
+                srv = legacy { mountpoint = "/srv"; };
+                opt = legacy { mountpoint = "/opt"; };
+
+                home = legacy {
+                    mountpoint = "/home";
+                    options.atime = "on";
+                };
+            };
+        };
+    };
+
+    # HACK: This allows sops-nix to read the file during stage 2,
+    # however its probably more correct to move the keyfile itself.
+    fileSystems."/home".neededForBoot = true;
+
+    # NOTE: Flattened for the installer script.
+    boot.initrd.systemd = { };
+    boot.initrd.kernelModules = [ ];
+    boot.initrd.availableKernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.blacklistedKernelModules = [ ];
+
+    boot.zfs.extraPools = [ "${config.networking.hostName}-rpool" ];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.grub.enable = lib.mkForce false;
+
+    system.stateVersion = "25.11";  # WARN: Changing this might break things. Just leave it.
+    networking.hostId = "48f674ed"; # Needed for ZFS machine identification.
+}
