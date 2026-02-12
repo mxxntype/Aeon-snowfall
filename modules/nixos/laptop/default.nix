5d49516d5bb7ea2f6aa70a263f9d3a9261050d7e
mxxntype <59417007+mxxntype@users.noreply.github.com>
2026-02-12 14:54:10 +0300

    feat(modules/laptop): TLP and related

--- modules/nixos/laptop/default.nix
+++ modules/nixos/laptop/default.nix
@@ -3,6 +3,22 @@
 {
     config = let cfg = config.aeon.hardware.meta;
     in lib.mkIf cfg.laptop {
+        # INFO: An amazing brightness control tool.
         hardware.brillo.enable = true;
+
+        powerManagement.powertop.enable = true;
+        services = {
+            power-profiles-daemon.enable = false;
+            tlp = {
+                enable = true;
+                settings = {
+                    CPU_BOOST_ON_AC = 0;
+                    CPU_BOOST_ON_BAT = 0;
+                    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+                    STOP_CHARGE_THRESH_BAT0 = 95;
+                };
+            };
+        };
     };
 }
