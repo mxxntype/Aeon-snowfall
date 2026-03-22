968c173a72125e9ab184332c65beb30d295348a3
mxxntype <59417007+mxxntype@users.noreply.github.com>
2026-03-22 21:39:38 +0300

    chore(systems/foghorn): Fix `hyprland` and WG stuff

--- homes/x86_64-linux/mxxntype@foghorn/default.nix
+++ homes/x86_64-linux/mxxntype@foghorn/default.nix
@@ -6,7 +6,7 @@ _: {
         wallpapers.enable = true;
         desktop.hyprland = {
             enable = true;
-            source = "nixpkgs";
+            source = "git";
         };
         
         monitors.monitors = [
