b4de1a7aed284005bfd16f9dded833a1a1f9939f
mxxntype <59417007+mxxntype@users.noreply.github.com>
2026-04-10 03:02:07 +0300

    fix(modules/prometheus): Use port `13000` instead of `3000`

--- modules/nixos/services/prometheus/default.nix
+++ modules/nixos/services/prometheus/default.nix
@@ -38,7 +38,7 @@
             settings = {
                 server = {
                     http_addr = "127.0.0.1";
-                    http_port = 3000;
+                    http_port = 13000;
                     enforce_domain = false;
                     enable_gzip = true;
                 };
