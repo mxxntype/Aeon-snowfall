0aa4705b7b6abc1fdcf82729129dfbbd3fffa4af
mxxntype <59417007+mxxntype@users.noreply.github.com>
2026-09-17 13:15:03 +0300

    chore(deps): Update `flake.lock` (try unstable nixpkgs)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/173b7e8' (2026-08-19)
      → 'github:nix-community/home-manager/afa6821' (2026-09-17)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/5880666' (2026-08-20)
      → 'github:nixos/nixpkgs/b1b8759' (2026-09-16)
    • Updated input 'unstable':
        'github:nixos/nixpkgs/56c02bc' (2026-08-23)
      → 'github:nixos/nixpkgs/b1b8759' (2026-09-16)

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
     inputs = {
         # SECTION: Core inputs.
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         unstable.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
